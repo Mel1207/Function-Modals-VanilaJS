ce231b8efc58617d2d5bf66ccb6ab02e64bc5ad5
Mel <meljohnpualon@gmail.com>
2021-06-10 22:54:24 +0800

    added function close for key buttons

--- app.js
+++ app.js
@@ -4,15 +4,21 @@
 const openBtn = document.querySelectorAll('.btn-open');
 const closeBtn = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
+const browserDoc = document;
 
+
+// EVENT FUNCTIONS
+const closeModal = () => {
+    overlay.classList.remove('show-modal');
+}
 
 const openModal = () => {
     overlay.classList.add('show-modal');
 }
 
-const closeModal = (e) => {
+const checkTarget = (e) => {
     if(e.target.classList.contains('show-modal') || e.target.classList.contains('close-modal')) {
-        overlay.classList.remove('show-modal');
+        closeModal()
     }
 }
 
@@ -20,5 +26,13 @@ openBtn.forEach(btn => {
     btn.addEventListener('click', openModal)
 });
 
+const escClose = e => {
+    if(e.key === 'Escape') {
+        closeModal()
+    }
+}
+
+// EVENT LISTENERS
+browserDoc.addEventListener('keydown', escClose)
 closeBtn.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal)
